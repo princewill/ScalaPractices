8cdfa03c3c0868168250e682a97b79c3f0aa3698
princewill ohuabunwa <wohuabun@gmail.com>
2020-05-01 12:55:32 -0400

    arr mani (not optimal)

--- src/main/scala/ArrayManipulation.scala
+++ src/main/scala/ArrayManipulation.scala
@@ -4,7 +4,19 @@ object ArrayManipulation {
 
   // Complete the arrayManipulation function below.
   def arrayManipulation(n: Int, queries: Array[Array[Int]]): Long = {
+    val arr = Array.fill[Long](10)(0)
+    var max = 0L
 
+    queries.foreach{ array => resolveQuery(array) }
+
+    def resolveQuery(query: Array[Int]): Unit = {
+      for(i <- (query.head -1) to (query(1) - 1)){
+        arr(i) = arr(i) + query(2)
+        if (arr(i) > max) max = arr(i) else max
+      }
+    }
+
+    max
   }
 
   def main(args: Array[String]) {
--- src/main/scala/Swaps.scala
+++ src/main/scala/Swaps.scala
@@ -4,12 +4,11 @@ object Swaps {
 
   // Complete the minimumSwaps function below.
   def minimumSwaps(arr: Array[Int]): Int = {
-    var a = 0
     var count = 0
 
     for(a <- arr.indices){
       while(a + 1 != arr(a)){
-        var temp = arr(a)
+        val temp = arr(a)
         arr(a) = arr(temp - 1)
         arr(temp - 1) = temp
         count = count + 1
